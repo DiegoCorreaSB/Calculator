0f8b8fbd257db5e2ff5dda47af819c800f44615d
Diego Corrêa <diegoberto2003@gmail.com>
2025-10-05 21:13:12 -0300

    layout da calculadora

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="main">
+        <div id='container-caulculator'>
+
+          <div id='container-view'>
+            <h1> 2 x 1 = 2</h1>
+          </div>
+
+          <div id="container-buttons">
+            <div id='container-numbers'>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button> 
+              <button>4</button>
+              <button>5</button>
+              <button>6</button> 
+              <button>1</button>
+              <button>2</button>
+              <button>3</button> 
+              <button>0</button>
+              <button>,</button>
+            </div>
+
+            <div id='container-operational'>
+              <button>÷</button>
+              <button>X</button>
+              <button>-</button>
+              <button>+</button>
+              <button>=</button>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
